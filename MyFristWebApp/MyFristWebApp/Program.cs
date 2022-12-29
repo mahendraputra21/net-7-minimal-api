1b45a03eff43446ffeaefc8f6542a929f4f4f239
dewa.mahendra <mahendraputra21@gmail.com>
2022-12-29 23:11:44 +0800

    update for docker

--- MyFristWebApp/MyFristWebApp/Program.cs
+++ MyFristWebApp/MyFristWebApp/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using MyFristWebApp.@class;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TodoDb>(
+builder.Services.AddDbContext<TodoDbContext>(
     opt => opt.UseInMemoryDatabase("Todos"));
 builder.Services.AddEndpointsApiExplorer();
 
@@ -42,25 +43,27 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello world Guys!");
 
-var todos = app.MapGroup("/todos").RequireAuthorization();
+//to enable Authorization
+var todos = app.MapGroup("/todos");
+    //.RequireAuthorization();
 
-todos.MapGet("/", (TodoDb db) => 
+todos.MapGet("/", (TodoDbContext db) => 
     db.Todos.ToListAsync());
 
-todos.MapPost("/", async (Todo todo, TodoDb db) =>
+todos.MapPost("/", async (Todo todo, TodoDbContext db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
     return TypedResults.Created($"/todos/{todo.Id}", todo);
 });
 
-todos.MapGet("/{id}", async Task<Results<Ok<Todo>, NotFound>>(int id, TodoDb db) =>
+todos.MapGet("/{id}", async Task<Results<Ok<Todo>, NotFound>>(int id, TodoDbContext db) =>
     await db.Todos.FindAsync(id) 
         is Todo todo
           ? TypedResults.Ok(todo)
           : TypedResults.NotFound());
 
-todos.MapPut("/{id}", async Task<Results<NotFound, NoContent>> (int id, TodoDb db, Todo inputTodo) => 
+todos.MapPut("/{id}", async Task<Results<NotFound, NoContent>> (int id, TodoDbContext db, Todo inputTodo) => 
 {
     var todo = await db.Todos.FindAsync(id);
     
@@ -75,7 +78,7 @@ todos.MapPut("/{id}", async Task<Results<NotFound, NoContent>> (int id, TodoDb d
     return TypedResults.NoContent();
 });
 
-todos.MapDelete("/{id}", async Task<Results<Ok<Todo>, NotFound>>(int id, TodoDb db) => 
+todos.MapDelete("/{id}", async Task<Results<Ok<Todo>, NotFound>>(int id, TodoDbContext db) => 
 { 
     if(await db.Todos.FindAsync(id) is Todo todo)
     {
@@ -88,20 +91,3 @@ todos.MapDelete("/{id}", async Task<Results<Ok<Todo>, NotFound>>(int id, TodoDb 
 });
 
 app.Run();
-
-///////////////////////////////////////////////////////////
-
-class Todo
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsCompleted { get; set; }
-}
-
-class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options) 
-        : base(options){ }
-
-    public DbSet<Todo> Todos => Set<Todo>();
-}
