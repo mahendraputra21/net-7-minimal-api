85c4292e8756f0ee4c2338ed839e89eba5b39a2f
dewa-mahendra <mahendraputra21@gmail.com>
2023-04-14 11:19:53 +0800

    un remark 2 test

--- TodoTestProject/Features/AddTodoTest.cs
+++ TodoTestProject/Features/AddTodoTest.cs
@@ -8,24 +8,24 @@ namespace TodoTestProject.Features
 {
     public class AddTodoTest : GetInMemory
     {
-        //[Fact]
-        //public async Task AddTodo_WithValidTodo_ReturnsCreated()
-        //{
-        //    // Arrange
-        //    var db = GetInMemoryDbContext();
-        //    var endpoints = new TodoEndpoints(db);
-        //    var todo = new Todo { Name = "Test Todo", IsCompleted = false };
+        [Fact]
+        public async Task AddTodo_WithValidTodo_ReturnsCreated()
+        {
+            // Arrange
+            var db = GetInMemoryDbContext();
+            var endpoints = new TodoEndpoints(db);
+            var todo = new Todo { Name = "Test Todo", IsCompleted = false };
 
-        //    // Act
-        //    var result = await endpoints.AddTodo(todo);
-        //    var actual = ((Created<Todo>)result.Result).Value;
+            // Act
+            var result = await endpoints.AddTodo(todo);
+            var actual = ((Created<Todo>)result.Result).Value;
 
-        //    // Assert
-        //    Assert.IsType<Results<Created<Todo>, NotFound>>(result);
-        //    Assert.Equal(1, await db.Todos.CountAsync());
-        //    Assert.Equal(todo, actual);
+            // Assert
+            Assert.IsType<Results<Created<Todo>, NotFound>>(result);
+            Assert.Equal(1, await db.Todos.CountAsync());
+            Assert.Equal(todo, actual);
 
-        //}
+        }
 
     }
 }
